463689ba091dd1ed68827f9efdb549a6ca5d6be7
RoJaeble <qgs0924@gmail.com>
2023-11-21 11:20:36 +0900

    :hammer: #4.4 Username Screen

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
+import 'package:tiktok_clone/features/authentication/username_screen.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 import 'package:tiktok_clone/shared/slide_route.dart';
@@ -15,7 +15,7 @@ class SignUpScreen extends StatelessWidget {
       );
 
   _onEmailTap(BuildContext context) => Navigator.of(context).push(
-        slideRoute(const EmailScreen()),
+        slideRoute(const UserNameScreen()),
       );
 
   @override
--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -1,10 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
-class UserNameScreen extends StatelessWidget {
+class UserNameScreen extends StatefulWidget {
   const UserNameScreen({super.key});
 
   @override
+  State<UserNameScreen> createState() => _UserNameScreenState();
+}
+
+class _UserNameScreenState extends State<UserNameScreen> {
+  final TextEditingController _usernamecontroller = TextEditingController();
+  String _username = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _usernamecontroller.addListener(() {
+      setState(() => _username = _usernamecontroller.text);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Sign up",
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "Create username",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            const Text(
+              "You can always change this later.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              controller: _usernamecontroller,
+              decoration: InputDecoration(
+                hintText: "Username",
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v16,
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
+                decoration: BoxDecoration(
+                  color: _username.isEmpty
+                      ? Colors.grey.shade300
+                      : Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(Sizes.size5),
+                ),
+                child: const Text(
+                  "Next",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+
+                  /// #4.4 05:56 : controller
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
