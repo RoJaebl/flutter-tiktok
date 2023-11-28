f0080650127257e65cdccf4415415cfd33f39276
RoJaebl <qgs0924@gmail.com>
2023-11-28 22:05:01 +0900

    :hammer: #9.2 TabBar

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = ["Top", "Users", "Videos", "Sounds", "Live", "Shopping", "Brands"];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("data"),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          title: const Text("Discover"),
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            tabAlignment: TabAlignment.start,
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
+            isScrollable: true,
+            unselectedLabelColor: Colors.grey.shade500,
+            labelColor: Colors.black,
+            indicatorColor: Colors.black,
+            indicatorSize: TabBarIndicatorSize.tab,
+            labelStyle: const TextStyle(
+              fontSize: Sizes.size16,
+              fontWeight: FontWeight.w600,
+            ),
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(
+                    fontSize: 28,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
