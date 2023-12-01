18e19abd5cab5459d3c6ac8d1d70608d4eaf680a
RoJaebl <qgs0924@gmail.com>
2023-12-01 14:36:52 +0900

    :hammer: #11.2 AnimatedList

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -11,6 +11,20 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(milliseconds: 300),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,48 +34,57 @@ class _ChatsScreenState extends State<ChatsScreen> {
         title: const Text("Direct messages"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
             ),
           ),
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(nikoaAvatarUri),
-              child: Text("헌남"),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "니꼬",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
+        itemBuilder:
+            (BuildContext context, int index, Animation<double> animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(nikoaAvatarUri),
+                  child: Text("헌남"),
                 ),
-                Text(
-                  "2:16 PM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size12,
-                  ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "니꼬 ($index)",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "2:16 PM",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+                subtitle: const Text(
+                  "Dopn't forget to make video",
+                ),
+              ),
             ),
-            subtitle: const Text(
-              "Dopn't forget to make video",
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
