5bce66fa662f7bace7f59894f15507a91a485243
RoJaebl <qgs0924@gmail.com>
2023-12-03 19:14:09 +0900

    :hammer: #14.2 kIsWeb

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
@@ -43,6 +44,7 @@ class _VideoPostState extends State<VideoPost>
   void _initVideoPlayer() async {
     await _videoPlayercontroller.initialize();
     await _videoPlayercontroller.setLooping(true);
+    kIsWeb ? await _videoPlayercontroller.setVolume(0) : null;
     _videoPlayercontroller.addListener(
       _onVideoChange,
     );
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await SystemChrome.setPreferredOrientations(
     [
-      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
     ],
   );
 
@@ -45,7 +46,7 @@ class TikTokApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const SignUpScreen(),
+      home: const MainNavigationScreen(),
     );
   }
 }
