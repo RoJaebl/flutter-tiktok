5b12ea2cc86ab50a6bdce69fab407ad76e97bcff
RoJaebl <qgs0924@gmail.com>
2023-12-19 19:26:18 +0900

    :hammer: #19.4 Flash Mode

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -19,6 +19,8 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   bool _micDenied = false;
 
   bool _isSelfieMode = false;
+  late FlashMode _flashMode;
+
   late CameraController _cameraController;
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       GlobalKey<RefreshIndicatorState>();
@@ -49,6 +51,8 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     );
 
     await _cameraController.initialize();
+
+    _flashMode = _cameraController.value.flashMode;
   }
 
   Future<void> initPermissions() async {
@@ -75,59 +79,110 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     setState(() {});
   }
 
+  Future<void> _setFlashMode(FlashMode newFlashMode) async {
+    await _cameraController.setFlashMode(newFlashMode);
+    _flashMode = newFlashMode;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: RefreshIndicator(
-          key: _refreshIndicatorKey,
-          color: Theme.of(context).primaryColor,
-          backgroundColor: Colors.white,
-          onRefresh: _checkPermission,
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: !_hasPermission || !_cameraController.value.isInitialized
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Initializing...",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: Sizes.size20,
-                        ),
+      backgroundColor: Colors.black,
+      body: RefreshIndicator(
+        key: _refreshIndicatorKey,
+        color: Theme.of(context).primaryColor,
+        backgroundColor: Colors.white,
+        onRefresh: _checkPermission,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: !_hasPermission || !_cameraController.value.isInitialized
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Initializing...",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size20,
                       ),
-                      Gaps.v20,
-                      ElevatedButton(
-                        onPressed: () => openAppSetting(),
-                        child: const Text("Open app settings"),
-                      ),
-                      ElevatedButton(
-                        onPressed: () =>
-                            _refreshIndicatorKey.currentState!.show(),
-                        child: const Text("Permission reflash"),
-                      ),
-                    ],
-                  )
-                : Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      CameraPreview(_cameraController),
-                      Positioned(
-                        top: Sizes.size20,
-                        left: Sizes.size20,
-                        child: IconButton(
-                          color: Colors.white,
-                          onPressed: toggleSelfieMode,
-                          icon: const Icon(
-                            Icons.cameraswitch,
+                    ),
+                    Gaps.v20,
+                    ElevatedButton(
+                      onPressed: () => openAppSetting(),
+                      child: const Text("Open app settings"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () =>
+                          _refreshIndicatorKey.currentState!.show(),
+                      child: const Text("Permission reflash"),
+                    ),
+                  ],
+                )
+              : Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CameraPreview(_cameraController),
+                    Positioned(
+                      top: Sizes.size32,
+                      right: Sizes.size20,
+                      child: Column(
+                        children: [
+                          IconButton(
+                            color: Colors.white,
+                            onPressed: toggleSelfieMode,
+                            icon: const Icon(
+                              Icons.cameraswitch,
+                            ),
                           ),
-                        ),
+                          Gaps.v10,
+                          IconButton(
+                            color: _flashMode == FlashMode.off
+                                ? Colors.amber.shade200
+                                : Colors.white,
+                            onPressed: () => _setFlashMode(FlashMode.off),
+                            icon: const Icon(
+                              Icons.flash_off_rounded,
+                            ),
+                          ),
+                          Gaps.v10,
+                          IconButton(
+                            color: _flashMode == FlashMode.always
+                                ? Colors.amber.shade200
+                                : Colors.white,
+                            onPressed: () => _setFlashMode(FlashMode.always),
+                            icon: const Icon(
+                              Icons.flash_on_rounded,
+                            ),
+                          ),
+                          Gaps.v10,
+                          IconButton(
+                            color: _flashMode == FlashMode.auto
+                                ? Colors.amber.shade200
+                                : Colors.white,
+                            onPressed: () => _setFlashMode(FlashMode.auto),
+                            icon: const Icon(
+                              Icons.flash_auto_rounded,
+                            ),
+                          ),
+                          Gaps.v10,
+                          IconButton(
+                            color: _flashMode == FlashMode.torch
+                                ? Colors.amber.shade200
+                                : Colors.white,
+                            onPressed: () => _setFlashMode(FlashMode.torch),
+                            icon: const Icon(
+                              Icons.flashlight_on_rounded,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-          ),
-        ));
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    );
   }
 }
