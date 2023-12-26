1c40368ce43bce425741417aa020fe2b50de5904
RoJaebl <qgs0924@gmail.com>
2023-12-26 16:42:12 +0900

    :hammer: #24.1 signOut

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -5,7 +5,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/common/widgets/theme_configuration/theme_config.dart';
+import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone/features/videos/view_models/playback_config_vm.dart';
 
 class SettingScreen extends ConsumerWidget {
@@ -156,7 +158,10 @@ class SettingScreen extends ConsumerWidget {
                       ),
                     ),
                     TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
+                      onPressed: () {
+                        ref.read(authRepo).signOut();
+                        context.go("/");
+                      },
                       child: const Text("Yes"),
                     ),
                   ],
