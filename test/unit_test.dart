234eccba0906ac6910de86e65d65e7e713d3e2c9
RoJaebl <qgs0924@gmail.com>
2024-01-17 00:06:52 +0900

    :white_check_mark: #30.3 Unit Testing

---
+++ test/unit_test.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tiktok_clone/features/videos/models/video_model.dart';
+import 'package:tiktok_clone/main.dart';
+
+void main() {
+  group("VideoModel Test", () {
+    test("Constructor", () {
+      final video = VideoModel(
+        id: "id",
+        title: "title",
+        description: "description",
+        fileUrl: "fileUrl",
+        thumbnailUrl: "thumbnailUrl",
+        creatorUid: "creatorUid",
+        likes: 1,
+        comments: 1,
+        createdAt: 1,
+        creator: "creator",
+      );
+      expect(video.id, "id");
+    });
+
+    test(".fromJson Constructor", () {
+      final video = VideoModel.fromJson(
+        json: {
+          "title": "title",
+          "description": "description",
+          "fileUrl": "fileUrl",
+          "thumbnailUrl": "thumbnailUrl",
+          "creatorUid": "creatorUid",
+          "likes": 1,
+          "comments": 1,
+          "createdAt": 1,
+          "creator": "creator",
+          "id": "id",
+        },
+        videoId: "videoId",
+      );
+      expect(video.title, "title");
+      expect(video.comments, isInstanceOf<int>());
+    });
+
+    test("toJson Method", () {
+      final video = VideoModel.fromJson(
+        json: {
+          "title": "title",
+          "description": "description",
+          "fileUrl": "fileUrl",
+          "thumbnailUrl": "thumbnailUrl",
+          "creatorUid": "creatorUid",
+          "likes": 1,
+          "comments": 1,
+          "createdAt": 1,
+          "creator": "creator",
+          "id": "id",
+        },
+        videoId: "videoId",
+      );
+      final json = video.toJson();
+
+      expect(json["id"], "videoId");
+      expect(json["likes"], isInstanceOf<int>());
+    });
+  });
+}
